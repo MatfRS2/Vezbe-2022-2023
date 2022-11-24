309d0431e1a36ebfe30bd5a1df6a98055a5c1ed3
ivan-ristovic <ivan.ristovic@oracle.com>
2022-11-24 18:26:22 +0100

    Add Ordering.Application

---
+++ Webstore/Services/Ordering/Ordering.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,28 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviors;
+
+public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly ILogger<UnhandledExceptionBehavior<TRequest, TResponse>> _logger;
+
+    public UnhandledExceptionBehavior(ILogger<UnhandledExceptionBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception e)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogError(e, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+            throw;
+        }
+    }
+}
