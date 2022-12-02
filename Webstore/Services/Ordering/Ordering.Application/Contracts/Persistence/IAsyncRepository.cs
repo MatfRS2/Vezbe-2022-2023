cf9c1997f0c357cffc04b605d643e8c511465f25
ivan-ristovic <ivan.ristovic@oracle.com>
2022-12-02 10:39:29 +0100

    Add service registration

--- Webstore/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ Webstore/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -6,16 +6,16 @@ namespace Ordering.Application.Contracts.Persistence;
 public interface IAsyncRepository<T> where T : AggregateRoot
 {
     Task<IReadOnlyList<T>> GetAllAsync();
-    Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+    Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>> predicate);
     Task<IReadOnlyList<T>> GetAsync(
         Expression<Predicate<T>> predicate,
-        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+        Expression<Func<IQueryable<T>, IOrderedQueryable<T>>> orderBy,
         string? includeString = null,
         bool disableTracking = true);
     Task<IReadOnlyList<T>> GetAsync(
         Expression<Predicate<T>> predicate,
+        Expression<Func<IQueryable<T>, IOrderedQueryable<T>>>? orderBy,
         List<Expression<Func<T, object>>> includes,
-        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
         bool disableTracking = true);
     Task<T> GetByIdAsync(int id);
     Task<T> AddAsync(T entity);
