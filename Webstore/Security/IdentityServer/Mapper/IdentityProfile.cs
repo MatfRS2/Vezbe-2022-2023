d00e6918273a30d8722bc814655b56942c03843f
ivan-ristovic <ivan.ristovic@oracle.com>
2022-12-14 12:05:16 +0100

    Add Identity: users, registration, login

---
+++ Webstore/Security/IdentityServer/Mapper/IdentityProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using IdentityServer.DTOs;
+using IdentityServer.Entities;
+
+namespace IdentityServer.Mapper;
+
+public class IdentityProfile : Profile
+{
+    public IdentityProfile()
+    {
+        CreateMap<User, NewUserDto>().ReverseMap();
+    }
+}
